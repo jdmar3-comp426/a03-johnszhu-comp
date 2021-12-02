282106d8ab9ecbe75b0eab6b26a7788026ab02f0
John Zhu <johnszhu@live.unc.edu>
2021-12-02 00:16:22 -0500

    allevensareOdd

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -178,8 +178,15 @@ export const filter = (arr, test) => {
  *   array and returns true only if all of the even elements in the array are
  *   odd numbers. Use the "everyEven" function in this function.
  */
+export function isOdd (num) {
+    if (!(num%2 == 0)) {
+        return true;
+    }
+    return false;
+}
+
 export const allEvensAreOdd = (arr) => {
-    
+    everyEven(arr, isOdd);
 };
 
 
